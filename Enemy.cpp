5d54a89fe3d54eb7ad9a67d2a79bdd291f10eec7
tpgns139 <42164539+tpgns139@users.noreply.github.com>
2020-01-30 15:39:01 +0900

    stageOne modify

--- Enemy.cpp
+++ Enemy.cpp
@@ -2,32 +2,32 @@
 #include "Enemy.h"
 #include"gameNode.h"
 
-Enemy::Enemy()
+Enemy::Enemy() //에너미이다.
 {
 }
 
-Enemy::~Enemy()
+Enemy::~Enemy() // 에너미가 사라진다
 {
 }
-HRESULT Enemy::init()
+HRESULT Enemy::init() // 에너미가 등장한다.
 {
-	cout << "EnemyInit" << endl;
+	cout << "EnemyInit" << endl; // 에너미는 끝났다
 	_tiles = SCENEMANAGER->getCurrentScene()->getTile();
 	return S_OK;
 }
 
 
-void Enemy::release()
+void Enemy::release() // 에너미가 죽었다
 {
 }
 
 
 
-void Enemy::update()
+void Enemy::update() // 에너미가 움직인다.
 {
 }
 
-void Enemy::setTile(vector<tagTile*> ins)
+void Enemy::setTile(vector<tagTile*> ins) // 에너미가 타일을 남기고 죽다.
 {
 	_tiles = ins;
 }
